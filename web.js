508ad7e39c1e6ce8f4350820e3c69083ed07fcef
emabi13 <eabiyo@gmail.com>
2013-07-15 13:59:49 +0000

    Update web.js with index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var fs = require('fs');
+    var j = fs.readFileSync('index.html');
+    response.send(j.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
