e1c120ada326e32a4736207be8e34d4e67a5ff18
Ubuntu <ubuntu@ip-172-31-39-113.us-west-2.compute.internal>
2013-07-23 00:15:49 +0000

    hw4 added

---
+++ hw4/web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var fs = require('fs');
+    var j = fs.readFileSync('index.html');
+
+    response.send(j.toString());
+});
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
